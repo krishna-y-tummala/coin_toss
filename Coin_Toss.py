d1222e10c7e792f8732c0f4ec06126f92cc2177e
Krishna Yashwanth Tummala <77645711+krishna-y-tummala@users.noreply.github.com>
2021-02-17 16:48:04 -0500

    Update Coin_Toss.py

--- Coin_Toss.py
+++ Coin_Toss.py
@@ -3,7 +3,7 @@ import random
 flips = []
 def coin_flip(num):
     
-    for i in range(num):
+    for toss_times in range(num):
         result = random.randint(1,2)
         
         if result == 1:
