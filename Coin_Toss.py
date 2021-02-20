456d9e3fcedf0eb51bb5104013c406379c3e30fb
Krishna Yashwanth Tummala <77645711+krishna-y-tummala@users.noreply.github.com>
2021-02-20 15:21:57 -0500

    Update Coin_Toss.py

--- Coin_Toss.py
+++ Coin_Toss.py
@@ -4,14 +4,22 @@ flips = []
 def coin_flip(num):
     
     for toss_times in range(num):
+
         result = random.randint(1,2)
-        
+        #Only one toss
+        if num == 1 and result ==1:
+            if result == 1:
+                print('It was Tails')
+            else:
+                print('It was Heads')
+            
+        #More than one toss    
         if result == 1:
             flips.append('T')
         elif result == 2:
             flips.append('H')
         else:
-            break   
+            pass   
     tail_count = flips.count('T')
     head_count = flips.count('H')
     print (f'Number of Tails:{tail_count}')
@@ -20,6 +28,5 @@ def coin_flip(num):
 
 flips = []
 num = int(input('How many times do you want to flip?: '))
-
 coin_flip(num)
 
